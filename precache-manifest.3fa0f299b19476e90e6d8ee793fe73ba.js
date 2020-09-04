311e582bfe945648201ccc06358ef05de5d98d4d
Abdul Haq Khalid <haq2682@gmail.com>
2020-09-04 19:12:49 +0500

    Updates

---
+++ precache-manifest.3fa0f299b19476e90e6d8ee793fe73ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf8c41d0950ac7b5dc3e12b84fedd81d",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "3f431390365d87474821",
+    "url": "/weatherapp/static/css/main.4366d961.chunk.css"
+  },
+  {
+    "revision": "5fae6b1de25c9e99106f",
+    "url": "/weatherapp/static/js/2.0258b798.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherapp/static/js/2.0258b798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f431390365d87474821",
+    "url": "/weatherapp/static/js/main.5926a00f.chunk.js"
+  },
+  {
+    "revision": "8862584b26a768bb4b89",
+    "url": "/weatherapp/static/js/runtime-main.cb4f0870.js"
+  },
+  {
+    "revision": "14144529e5ba9a0590b4abe60367f8db",
+    "url": "/weatherapp/static/media/app.14144529.jpg"
+  }
+]);
